ca6ddd6277bfc925d054278d5713753029e273c2
Nroulston <nicolas.roulston@gmail.com>
2020-11-04 20:12:31 -0700

    prepare for monkey patch of String Class

--- palindrome.rb
+++ palindrome.rb
@@ -4,28 +4,7 @@ class Phrase < String
    processed_content == processed_content.reverse
     end
 
-  def louder
-    self.content.upcase
-  end
-
-  def processor(string)
+  def processed_content
     self.downcase
-  end
-
-  def processed_content
-    processor(self)
-  end
-end
-
-class TranslatedPhrase < Phrase
-  attr_accessor :translation
-
-  def initialize(content, translation)
-    super(content)
-    @translation = translation
-  end
-
-  def processed_content
-    processor(translation)
   end
 end
